3ce4dbf85437c4f5b035ffab4e2e8e66d7ffd2db
ErikaCV <erikajohanacruzvallejo@gmail.com>
2023-07-21 17:15:14 -0300

    fix: :bug:

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,15 +1,15 @@
-import Slider  from './Slider'
-import Sales from './Sales'
-import Banner from './Banner'
+import Slider from "./Slider";
+import Sales from "./Sales";
+import Banner from "./Banner";
 
-function HomePage () {
-    return (
-        <div>
-         <Slider />
-         <Sales />
-         <Banner />
-        </div>
-       )
+function HomePage() {
+  return (
+    <div>
+      <Slider />
+      <Sales />
+      <Banner />
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
