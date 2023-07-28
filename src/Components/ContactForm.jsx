5cb1c20be469881247c7c5e574ee27f55139a1da
ErikaCV <erikajohanacruzvallejo@gmail.com>
2023-07-28 12:49:04 -0300

    feat: :zap: add contact form

---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,97 @@
+import { useForm, Controller } from 'react-hook-form';
+
+const ContactForm = () => {
+  const { handleSubmit, control, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>CONTACTATE CON ......!</h1>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <label>NOMBRE</label>
+            <Controller
+              name="nombre"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'El nombre es requerido', maxLength: 15 }}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type="text"
+                  className="form-control required"
+                />
+              )}
+            />
+            {errors.nombre && <p>{errors.nombre.message}</p>}
+
+            <label>APELLIDO</label>
+            <Controller
+              name="apellido"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'El apellido es requerido', maxLength: 15 }}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type="text"
+                  className="form-control required"
+                />
+              )}
+            />
+            {errors.apellido && <p>{errors.apellido.message}</p>}
+
+            <label>EMAIL</label>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: 'El email es requerido',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: 'Dirección de correo inválida',
+                },
+              }}
+              render={({ field }) => (
+                <input
+                  {...field}
+                  type="email"
+                  className="form-control required"
+                />
+              )}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+
+            <label>DEJANOS UN MENSAJE!</label>
+            <Controller
+              name="mensaje"
+              control={control}
+              defaultValue=""
+              rules={{ required: 'El mensaje es requerido', maxLength: 250 }}
+              render={({ field }) => (
+                <textarea
+                  {...field}
+                  className="form-control required"
+                />
+              )}
+            />
+            {errors.mensaje && <p>{errors.mensaje.message}</p>}
+
+            <button type="submit">ENVIAR</button>
+          </form>
+        </div>
+        <div className="col">
+          <h1>ENCUENTRANOS EN:</h1>
+          <div className="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1023707775184!2d-65.20939048529601!3d-26.83669609650096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses!2sar!4v1677371497043!5m2!1ses!2sar" style={{border:0}} allowFullScreen loading="lazy" title="map"></iframe>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ContactForm;
+
