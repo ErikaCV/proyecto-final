cd5c9940b267067f8e318a32734b740059accd77
ErikaCV <erikajohanacruzvallejo@gmail.com>
2023-08-13 19:37:54 -0300

    fix: :lipstick: added style contact form

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -8,9 +8,9 @@ const ContactForm = () => {
   return (
     <div className="cf-container">
       <div className="cf-row">
-        <div className="cf-col">
-          <h2 className="cf-title">CONTACTATE CON ......!</h2>
-          <form className="cf-form" onSubmit={handleSubmit(onSubmit)}>
+        <div className="cf-col col">
+          <form className="cf-form needs-validation-contact" onSubmit={handleSubmit(onSubmit)}>
+          <h2 className="cf-title">CONTACTATE CON NOSOTROS!</h2>
             <label>NOMBRE</label>
             <Controller
               name="nombre"
@@ -82,12 +82,12 @@ const ContactForm = () => {
             />
             {errors.mensaje && <p>{errors.mensaje.message}</p>}
 
-            <button type="submit">ENVIAR</button>
+            <button className='btn-secondary ' type="submit">ENVIAR</button>
           </form>
         </div>
-        <div className="cf-col">
+        <div className=" cf-coll cfm needs-validation-contact">
+          <div className="cf-map map-container map">
           <h2>ENCUENTRANOS EN:</h2>
-          <div className="cf-map">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.1023707775184!2d-65.20939048529601!3d-26.83669609650096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses!2sar!4v1677371497043!5m2!1ses!2sar" style={{border:0, width:"90%", height:"100%"}} allowFullScreen loading="lazy" title="map"></iframe>
           </div>
         </div>
