07eeb5728ecfb56bdf8b7a372cad3688479a9ae8
francojnieva <franco97nieva@hotmail.com>
2023-08-13 01:09:45 -0300

    feat: add new banner images and change the content about Sales

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
-import moto1 from '../assets/images/moto1.jpg';
 import '../styles/Sales.css'
 import OfferCarousel from './OfferCarousel';
+import banner from "../assets/images/img-slider/banner.jpg"
+import banner2 from "../assets/images/img-slider/banner2.webp"
+import { Link } from 'react-router-dom';
+
 function Sales() {
   return (
-    <div className="wrapper mt-4 ">
-        <div className="row-one rounded">
-      <OfferCarousel/>
-        </div>
-
-      <div className="row-two rounded">
-      <OfferCarousel/>
+    <div className="wrapper mt-4 container-banner ">
+      <div className="row-one rounded bb">
+        <OfferCarousel/>
       </div>
-      <div className="col-one rounded">
-      <h1>Aqui iria una imagen</h1>
-      <OfferCarousel/>
-      
+      {/* <div className="row-two rounded">
+        <OfferCarousel/>
+      </div> */}
+      <div className='banner-container'>
+        {/* <h1>Aqui iria una imagen</h1> */}
+        {/* <OfferCarousel/> */}
+        <Link to="products/64d80909ac95a9aa7c386961">
+          <img  className='banner my-2' src={banner} alt="banner de guantes" />
+        </Link>
+        <Link to="products/64d80751ac95a9aa7c386951">
+          <img  className='banner my-2' src={banner2} alt="banner de camperas" />
+        </Link>
       </div>
     </div>
   );
