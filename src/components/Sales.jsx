33ec9458e706d345399a0f8d98a6925ce01efc37
francojnieva <franco97nieva@hotmail.com>
2023-08-14 00:20:02 -0300

    feat: add new banner images

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -7,23 +7,25 @@ import { Link } from 'react-router-dom';
 
 function Sales() {
   return (
-    <div className="wrapper mt-4 container-banner ">
-      <div className="row-one rounded bb">
+    <div className="row mt-4 d-flex justify-content-center aling-items-center flex-column flex-lg-row container-fluid m-0">
+      {/* <div className="row-one rounded bb">
         <OfferCarousel/>
-      </div>
+      </div> */}
       {/* <div className="row-two rounded">
         <OfferCarousel/>
       </div> */}
-      <div className='banner-container'>
         {/* <h1>Aqui iria una imagen</h1> */}
         {/* <OfferCarousel/> */}
-        <Link to="products/64d80909ac95a9aa7c386961">
-          <img  className='banner my-2' src={banner} alt="banner de guantes" />
-        </Link>
-        <Link to="products/64d80751ac95a9aa7c386951">
-          <img  className='banner my-2' src={banner2} alt="banner de camperas" />
-        </Link>
-      </div>
+        <div className='col-12 col-lg-6'>
+          <Link to="products/64d80909ac95a9aa7c386961">
+            <img  className='banner my-2' src={banner} alt="banner de guantes" />
+          </Link>
+        </div>
+        <div className='col-12 col-lg-6'>
+          <Link to="products/64d80751ac95a9aa7c386951">
+            <img  className='banner my-2' src={banner2} alt="banner de camperas" />
+          </Link>
+        </div>
     </div>
   );
 }
